596318e0dccc41e85bb3a866efa4e4570a84402b
Aatish Gore <aatish.gore@wwindia.com>
2020-02-28 17:33:41 +0530

    feat: adding file server location

--- classes/storage.ts
+++ classes/storage.ts
@@ -4,7 +4,7 @@ import { errorMessage } from '../interfaces/storageInterface';
 
 export default class Storage {
   private static instance: Storage | null;
-  private fileLocation: string = './attachment/';
+  private fileLocation: string = process.env.attachment;
   private status: errorMessage | undefined = undefined
   static getInstance() {
     if (!this.instance) {
